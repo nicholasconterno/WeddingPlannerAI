0a500807abcb83711e64c91c840dbbae0344e7ba
nicholasconterno <nec21@duke.edu>
2024-08-07 00:53:30 -0700

    added some more comments

--- flask_app.py
+++ flask_app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    '''
+    """
     Home page
-    '''
+    """
     return render_template("index.html")
 
 
 @app.route("/query", methods=["POST"])
 def query():
-    '''
-    Query the AI model'''
+    """
+    Query the AI model"""
     data = request.get_json()
     user_input = data.get("input_text")
     # get venfor email from the form
@@ -32,16 +32,16 @@ def query():
 
 @app.route("/summary", methods=["GET"])
 def summary():
-    '''
-    Get the summary of the information gathered'''
+    """
+    Get the summary of the information gathered"""
     summary = get_summary("sk-no-key-required")
     return jsonify({"summary": summary})
 
 
 @app.route("/next_step", methods=["GET"])
 def next_step():
-    '''
-    Get the next step for the bride based on the summary of the information gathered'''
+    """
+    Get the next step for the bride based on the summary of the information gathered"""
     next_step = get_next_step_for_bride("sk-no-key-required")
     return jsonify({"next_step": next_step})
 
